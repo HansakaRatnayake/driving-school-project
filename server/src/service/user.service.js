11d49dcab057f53a7efd84ec9c9e45c1fd981962
hansakarathnayake <hndk2002@gmail.com>
2024-09-18 02:48:26 +0530

    Build:search by role

--- server/src/service/user.service.js
+++ server/src/service/user.service.js
@@ -1,7 +1,8 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Types;
 const User = require('../model/user.model');
 const userRepo = require('../repository/user.repository');
 const { createUser, findAllUsers, findUserByUsername, updateUser, deleteUser } = userRepo;
-
 
 const create = async (user) => {
 
@@ -21,13 +22,24 @@ const create = async (user) => {
 
 const findAll = async (queryparamobject) => {
 
-    const {firstname, lastname, username} = queryparamobject;
+    const {firstname, lastname, username, role} = queryparamobject;
 
     const query = {};
 
     if(firstname && firstname.trim() !== '') query.firstname = { $regex: firstname, $options: 'i' };
     if(lastname && lastname.trim() !== '') query.lastname = { $regex: lastname, $options: 'i' };
     if(username && username.trim() !== '') query.username = { $regex: username, $options: 'i' };
+    if(role && role.trim() !== '') {
+        if(ObjectId.isValid(role)) {
+            query.role = new ObjectId(role);
+        }else{
+            return {
+                data: "Invalid ObjectId",
+                statuscode: 404
+            }
+        }
+       
+    }
 
     try {
         const resData = await findAllUsers(query);
