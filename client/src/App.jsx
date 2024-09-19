df5e13d58a07caa4de7f7a36dbe69ccfe31f4f15
hansakarathnayake <hndk2002@gmail.com>
2024-09-19 20:36:18 +0530

    User Module UI finalized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
@@ -27,7 +27,7 @@ import Navbar from './components/Navbar/Navbar';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import ProtectedRoute from './ProtectedRoute';
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Profile from "./modules/Profile.jsx";
 import Admin from "./modules/Admin.jsx";
 import User from "./modules/User/User.jsx";
@@ -40,15 +40,12 @@ import TraineesAddForm from "./modules/Trainees/TraineesAddForm.jsx";
 import TraineesUpdateForm from "./modules/Trainees/TraineesUpdateForm.jsx";
 import TrainersAddForm from "./modules/Trainers/TrainersAddForm.jsx";
 
-
 export const UserContext = createContext(null);
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
-  const [user,setUser] = useState();
+  const [user, setUser] = useState();
   const element = document.documentElement;
-
-  
 
   // AOS Initialization
   useEffect(() => {
@@ -71,76 +68,75 @@ const App = () => {
       localStorage.setItem('theme', 'light');
     }
 
-    if(localStorage.getItem("auth_user")){
+    if (localStorage.getItem("auth_user")) {
       setUser(JSON.parse(localStorage.getItem("auth_user")));
-  }
-
+    }
   }, [theme]);
 
-  
-
   return (
-    <UserContext.Provider value={{user,setUser}}>
-      <Toaster position="top-right"/>
-    <Router>
-      <Navbar theme={theme} setTheme={setTheme} />
-
-      <Routes>
-
-        <Route path='/' element={<Landing />} />
-        <Route
-          path="/home"
-          element={
-            <>
-              <Hero theme={theme} setTheme={setTheme} />
-              <About />
-              <Services />
-            </>
-          }
-        />
-        {/*Admin Routes*/}
-        <Route path="/admin" element={ <Admin /> }>
-          <Route path="" element={ <Dashboard/> }/>
-          <Route path="user" element={ <User/> }/>
-          <Route path="user-add" element={ <UserForm/> }/>
-
-          <Route path="trainee" element={ <Trainees/> }/>
-          <Route path="trainee-add" element={ <TraineesAddForm/> }/>
-          <Route path="trainee-update/:username" element={ <TraineesUpdateForm/> }/>
-
-          <Route path="trainings" element={ <Trainings/> }/>
-
-          <Route path="trainers" element={ <Trainers/> }/>
-          <Route path="trainers-add" element={ <TrainersAddForm/> }/>
-
-        </Route>
-
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/information" element={<Information />} />
-        <Route path="/storepage" element={<StorePage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about1" element={<About1 />} />
-        <Route path="/booking" element={<ProtectedRoute><Booking /></ProtectedRoute>} />
-        <Route path="/newlicence" element={<NewLicence />} />
-        <Route path="/drivingrules" element={<DrivingRules />} />
-        <Route path="/drivinginfo" element={<DrivingInfo />} />
-        <Route path="/speciallicenseinfo" element={<SpecialLicenseInfo />} />
-        <Route path="/revenulicence" element={<RevenuLicence />} />
-        <Route path="/highwayoffenses" element={<HighwayOffenses />} />
-        <Route path="/expressway" element={<Expressway />} />
-        <Route path="/foreignlicence" element={<ForeignLicence />} />
-        <Route path="/oldlicence" element={<OldLicence />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/assesment" element={<Assesment />} />
-        <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>} />
+    <UserContext.Provider value={{ user, setUser }}>
+      <Toaster position="top-right" />
+      <Router>
+        <div className="w-[100vw] h-[100vh] flex flex-col overflow-y-hidden">
         
-      </Routes>
-    </Router>
-    </UserContext.Provider> 
+        {/* Navbar remains fixed at the top */}
+        <Navbar theme={theme} setTheme={setTheme} />
+
+        {/* Scrollable content area */}
+        <div style={{ flex: 1, overflowY: 'auto', height: 'calc(100vh - 64px)' }}>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route
+              path="/home"
+              element={
+                <>
+                  <Hero theme={theme} setTheme={setTheme} />
+                  <About />
+                  <Services />
+                </>
+              }
+            />
+
+            {/* Admin Routes */}
+            <Route path="/admin" element={<Admin />}>
+              <Route path="" element={<Dashboard />} />
+              <Route path="user" element={<User />} />
+              <Route path="user-add" element={<UserForm />} />
+              <Route path="trainee" element={<Trainees />} />
+              <Route path="trainee-add" element={<TraineesAddForm />} />
+              <Route path="trainee-update/:username" element={<TraineesUpdateForm />} />
+              <Route path="trainings" element={<Trainings />} />
+              <Route path="trainers" element={<Trainers />} />
+              <Route path="trainers-add" element={<TrainersAddForm />} />
+            </Route>
+
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/information" element={<Information />} />
+            <Route path="/storepage" element={<StorePage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about1" element={<About1 />} />
+            <Route path="/booking" element={<ProtectedRoute><Booking /></ProtectedRoute>} />
+            <Route path="/newlicence" element={<NewLicence />} />
+            <Route path="/drivingrules" element={<DrivingRules />} />
+            <Route path="/drivinginfo" element={<DrivingInfo />} />
+            <Route path="/speciallicenseinfo" element={<SpecialLicenseInfo />} />
+            <Route path="/revenulicence" element={<RevenuLicence />} />
+            <Route path="/highwayoffenses" element={<HighwayOffenses />} />
+            <Route path="/expressway" element={<Expressway />} />
+            <Route path="/foreignlicence" element={<ForeignLicence />} />
+            <Route path="/oldlicence" element={<OldLicence />} />
+            <Route path="/landing" element={<Landing />} />
+            <Route path="/assesment" element={<Assesment />} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          </Routes>
+        </div>
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 };
 
