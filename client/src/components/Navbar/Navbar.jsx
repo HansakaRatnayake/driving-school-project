ad8773aa3248c25bf6080f05d2c3ba7830b5df8e
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2024-09-23 11:13:06 +0530

    build: Navbar Dropdown

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -69,20 +69,36 @@ const Navbar = ({ theme, setTheme }) => {
 
             {/* User Login Icon */}
             {user ?
-                <div className='text-orange-500'>
-                  <div className='bg-black size-6 rounded-full' onClick={()=>document.getElementById('my_modal_1').showModal()}>
-                    {/* {loggeduser.username} */}
-                  </div>
+                <div className='text-black mt-1'>
+
+                    <div className="dropdown">
+                        <div tabIndex={0} className="size-6 dark:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"/>
+                            </svg>
+                        </div>
+                        <ul tabIndex={0}
+                            className="dropdown-content menu bg-base-100 dark:bg-black dark:text-white rounded-box z-[1] w-52 p-2 shadow">
+                            <li><Link to="profile">
+                                <div>Profile</div>
+                            </Link></li>
+                            <li>
+                                <div onClick={() => document.getElementById('my_modal_1').showModal()}>Logout</div>
+                            </li>
+                        </ul>
+                    </div>
 
                 </div> :
                 <Link to="/login" className="text-2xl">
-                  {theme === 'dark' ? <FaRegUser/> : <FaUser/>}
+                    {theme === 'dark' ? <FaRegUser/> : <FaUser/>}
                 </Link>
             }
 
 
-            {/* Mobile Menu Toggle Icon */}
-            <button onClick={toggleMenu} className="md:hidden text-2xl">
+              {/* Mobile Menu Toggle Icon */}
+              <button onClick={toggleMenu} className="md:hidden text-2xl">
               {showMenu ? <HiMenuAlt1/> : <HiMenuAlt3/>}
             </button>
           </div>
