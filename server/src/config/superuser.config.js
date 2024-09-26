84eee34b43a9694ab2ece67327d58d47dc24179d
hansakarathnayake <hndk2002@gmail.com>
2024-09-27 00:32:36 +0530

    build initeal roles | inital statuses

--- server/src/config/superuser.config.js
+++ server/src/config/superuser.config.js
@@ -6,25 +6,56 @@ const createSuperUserAdmin = async () => {
 
     try {
 
-        
+        if(!await Role.findOne({name:"SUPER_ADMIN"})) {
+            const superadminrole = await new Role({
+                name:"SUPER_ADMIN"
+            }).save();
+        }
+
+        if(!await Role.findOne({name:"ADMIN"})) {
+            const superadminrole = await new Role({
+                name:"ADMIN"
+            }).save();
+        }
+
+        if(!await Role.findOne({name:"USER"})) {
+            const superadminrole = await new Role({
+                name:"USER"
+            }).save();
+        }
+
+        if(!await UserStatus.findOne({name:"Active"})) {
+            const superadminrole = await new UserStatus({
+                name:"Active"
+            }).save();
+        }
+
+        if(!await UserStatus.findOne({name:"Inactive"})) {
+            const superadminrole = await new UserStatus({
+                name:"Inactive"
+            }).save();
+        }
 
         const role = await Role.findOne({name:"SUPER_ADMIN"});
         const status = await UserStatus.findOne({name:"Active"});
 
+        setTimeout(async()=>{
+            if(!await User.findOne({role:role._id})) {
+                const superuser = new User({
+                    firstname:'Super',
+                    lastname:'Admin',
+                    username:'superuseradmin@gmail.com',
+                    password:'superuseradmin1234',
+                    canDelete:false,
+                    userstatus:status._id,
+                    role:role._id
+                }).save(); 
+            }
 
-        if(!await User.findOne({role:role._id})) {
-            const superuser = new User({
-                firstname:'Super',
-                lastname:'Admin',
-                username:'superuseradmin@gmail.com',
-                password:'superuseradmin1234',
-                canDelete:false,
-                userstatus:status._id,
-                role:role._id
-            }).save(); 
-        }
+        },1000)
+       
     }catch(err){
-        console.error("Error while initiating superuser");
+        console.error("Error while initiating superuser" + err);
 
     }
 }
